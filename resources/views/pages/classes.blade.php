7057eec8f5036bfc8a58e21a17663ed36cdc29f8
cboxone <logossoumichel@gmail.com>
2024-04-17 15:49:32 +0100

    de

--- resources/views/pages/classes.blade.php
+++ resources/views/pages/classes.blade.php
@@ -76,17 +76,8 @@
             <p class="card-description" style="color: black">
               Eleve
             </p>
+
             <div class="form-group">
-              <label>Single select box using select 2</label>
-              <select class="js-example-basic-single w-100">
-                <option value="AL">Alabama</option>
-                <option value="WY">Wyoming</option>
-                <option value="AM">America</option>
-                <option value="CA">Canada</option>
-                <option value="RU">Russia</option>
-              </select>
-            </div>
-            <div class="form-group w-100">
               <select class="js-example-basic-multiple w-100" multiple="multiple">
                 @foreach ($eleve as $eleves)
                   <option value="{{$eleves->NOM}} {{$eleves->PRENOM}}">{{$eleves->NOM}} {{$eleves->PRENOM}}</option>
@@ -139,7 +130,7 @@
                   <div class="col">
                     <label>Date</label>
                     <div id="the-basics">
-                      <input class="typeahead" type="date" placeholder="States of USA">
+                      <input class="typeahead" type="date" placeholder="">
                     </div>
                   </div>
                   <div class="col">
