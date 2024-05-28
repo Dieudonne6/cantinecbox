a4b93219e35c790641438e8513f6a48b0b87e734
Pascaline Dah Zinsou <pascaline@wazoom-studio.com>
2024-05-28 10:41:27 +0100

    Update.

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,175 +1,4 @@
-<<<<<<< HEAD
 <nav class="sidebar sidebar-start" id="sidebar" data-darkreader-mode="dynamic" >
-=======
-{{-- <div class="theme-setting-wrapper">
-  <div id="settings-trigger"><i class="typcn typcn-cog-outline"></i></div>
-  <div id="theme-settings" class="settings-panel">
-    <i class="settings-close typcn typcn-times"></i>
-    <p class="settings-heading">SIDEBAR SKINS</p>
-    <div class="sidebar-bg-options selected" id="sidebar-light-theme"><div class="img-ss rounded-circle bg-light border mr-3"></div>Light</div>
-    <div class="sidebar-bg-options" id="sidebar-dark-theme"><div class="img-ss rounded-circle bg-dark border mr-3"></div>Dark</div>
-    <p class="settings-heading mt-2">HEADER SKINS</p>
-    <div class="color-tiles mx-0 px-4">
-      <div class="tiles success"></div>
-      <div class="tiles warning"></div>
-      <div class="tiles danger"></div>
-      <div class="tiles info"></div>
-      <div class="tiles dark"></div>
-      <div class="tiles default"></div>
-    </div>
-  </div>
-</div> --}}
-{{-- <div id="right-sidebar" class="settings-panel">
-  <i class="settings-close typcn typcn-times"></i>
-  <ul class="nav nav-tabs" id="setting-panel" role="tablist">
-    <li class="nav-item">
-      <a class="nav-link active" id="todo-tab" data-toggle="tab" href="#todo-section" role="tab" aria-controls="todo-section" aria-expanded="true">TO DO LIST</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" id="chats-tab" data-toggle="tab" href="#chats-section" role="tab" aria-controls="chats-section">CHATS</a>
-    </li>
-  </ul>
-  <div class="tab-content" id="setting-content">
-    <div class="tab-pane fade show active scroll-wrapper" id="todo-section" role="tabpanel" aria-labelledby="todo-section">
-      <div class="add-items d-flex px-3 mb-0">
-        <form class="form w-100">
-          <div class="form-group d-flex">
-            <input type="text" class="form-control todo-list-input" placeholder="Add To-do">
-            <button type="submit" class="add btn btn-primary todo-list-add-btn" id="add-task">Add</button>
-          </div>
-        </form>
-      </div>
-      <div class="list-wrapper px-3">
-        <ul class="d-flex flex-column-reverse todo-list">
-          <li>
-            <div class="form-check">
-              <label class="form-check-label">
-                <input class="checkbox" type="checkbox">
-                Team review meeting at 3.00 PM
-              </label>
-            </div>
-            <i class="remove typcn typcn-delete-outline"></i>
-          </li>
-          <li>
-            <div class="form-check">
-              <label class="form-check-label">
-                <input class="checkbox" type="checkbox">
-                Prepare for presentation
-              </label>
-            </div>
-            <i class="remove typcn typcn-delete-outline"></i>
-          </li>
-          <li>
-            <div class="form-check">
-              <label class="form-check-label">
-                <input class="checkbox" type="checkbox">
-                Resolve all the low priority tickets due today
-              </label>
-            </div>
-            <i class="remove typcn typcn-delete-outline"></i>
-          </li>
-          <li class="completed">
-            <div class="form-check">
-              <label class="form-check-label">
-                <input class="checkbox" type="checkbox" checked>
-                Schedule meeting for next week
-              </label>
-            </div>
-            <i class="remove typcn typcn-delete-outline"></i>
-          </li>
-          <li class="completed">
-            <div class="form-check">
-              <label class="form-check-label">
-                <input class="checkbox" type="checkbox" checked>
-                Project review
-              </label>
-            </div>
-            <i class="remove typcn typcn-delete-outline"></i>
-          </li>
-        </ul>
-      </div>
-      <div class="events py-4 border-bottom px-3">
-        <div class="wrapper d-flex mb-2">
-          <i class="typcn typcn-media-record-outline text-primary mr-2"></i>
-          <span>Feb 11 2018</span>
-        </div>
-        <p class="mb-0 font-weight-thin text-gray">Creating component page</p>
-        <p class="text-gray mb-0">build a js based app</p>
-      </div>
-      <div class="events pt-4 px-3">
-        <div class="wrapper d-flex mb-2">
-          <i class="typcn typcn-media-record-outline text-primary mr-2"></i>
-          <span>Feb 7 2018</span>
-        </div>
-        <p class="mb-0 font-weight-thin text-gray">Meeting with Alisa</p>
-        <p class="text-gray mb-0 ">Call Sarah Graves</p>
-      </div>
-    </div>
-    <!-- To do section tab ends -->
-    <div class="tab-pane fade" id="chats-section" role="tabpanel" aria-labelledby="chats-section">
-      <div class="d-flex align-items-center justify-content-between border-bottom">
-        <p class="settings-heading border-top-0 mb-3 pl-3 pt-0 border-bottom-0 pb-0">Friends</p>
-        <small class="settings-heading border-top-0 mb-3 pt-0 border-bottom-0 pb-0 pr-3 font-weight-normal">See All</small>
-      </div>
-      <ul class="chat-list">
-        <li class="list active">
-          <div class="profile"><img src="../../images/faces/face1.jpg" alt="image"><span class="online"></span></div>
-          <div class="info">
-            <p>Thomas Douglas</p>
-            <p>Available</p>
-          </div>
-          <small class="text-muted my-auto">19 min</small>
-        </li>
-        <li class="list">
-          <div class="profile"><img src="../../images/faces/face2.jpg" alt="image"><span class="offline"></span></div>
-          <div class="info">
-            <div class="wrapper d-flex">
-              <p>Catherine</p>
-            </div>
-            <p>Away</p>
-          </div>
-          <div class="badge badge-success badge-pill my-auto mx-2">4</div>
-          <small class="text-muted my-auto">23 min</small>
-        </li>
-        <li class="list">
-          <div class="profile"><img src="../../images/faces/face3.jpg" alt="image"><span class="online"></span></div>
-          <div class="info">
-            <p>Daniel Russell</p>
-            <p>Available</p>
-          </div>
-          <small class="text-muted my-auto">14 min</small>
-        </li>
-        <li class="list">
-          <div class="profile"><img src="../../images/faces/face4.jpg" alt="image"><span class="offline"></span></div>
-          <div class="info">
-            <p>James Richardson</p>
-            <p>Away</p>
-          </div>
-          <small class="text-muted my-auto">2 min</small>
-        </li>
-        <li class="list">
-          <div class="profile"><img src="../../images/faces/face5.jpg" alt="image"><span class="online"></span></div>
-          <div class="info">
-            <p>Madeline Kennedy</p>
-            <p>Available</p>
-          </div>
-          <small class="text-muted my-auto">5 min</small>
-        </li>
-        <li class="list">
-          <div class="profile"><img src="../../images/faces/face6.jpg" alt="image"><span class="online"></span></div>
-          <div class="info">
-            <p>Sarah Graves</p>
-            <p>Available</p>
-          </div>
-          <small class="text-muted my-auto">47 min</small>
-        </li>
-      </ul>
-    </div>
-  </div>
-</div> --}}
-
-<nav class="sidebar sidebar-offcanvas" id="sidebar">
->>>>>>> 111e19fff1907e77c835374c364e6d247aa491f1
   <ul class="nav">
 
     {{-- <li class="nav-item">
@@ -185,14 +14,9 @@
         <span class="menu-title">Inscriptions & disciplines</span>
         <i class="menu-arrow"></i>
       </a>
-<<<<<<< HEAD
 
        <div class="collapse" id="ui-basic">
          {{-- Acceuil --}}
-=======
-      <div class="collapse" id="ui-basic">
-         {{-- Acceuil 
->>>>>>> 111e19fff1907e77c835374c364e6d247aa491f1
 
         <div class="dropdown">
           <button class="btn btn-light-lg dropdown-toggle" type="button" id="dropdownMenuSizeButton1" data-toggle="dropdown" 
@@ -214,12 +38,7 @@
             <a class="dropdown-item text-center" href="#" _msttexthash="261807" _msthash="282" _mstvisible="1">Table des classes</a>
             <a class="dropdown-item text-center" href="#" _msttexthash="229879" _msthash="283" _mstvisible="1">Couper</a>
           </div>
-<<<<<<< HEAD
         </div>
-=======
-        </div> --}}
-  
->>>>>>> 111e19fff1907e77c835374c364e6d247aa491f1
          {{-- Scolarité --}}
         
         {{-- <div class="dropdown">
@@ -233,23 +52,13 @@
             <a class="dropdown-item text-center" href="#" _msttexthash="261807" _msthash="282" _mstvisible="1">Paiement des non inscrits</a>
             <a class="dropdown-item text-center" href="#" _msttexthash="76466" _msthash="281" _mstvisible="1">Duplicata</a>
           </div>
-<<<<<<< HEAD
         </div>
-=======
-        </div> --}}
-
->>>>>>> 111e19fff1907e77c835374c364e6d247aa491f1
          {{-- Dicipline --}}
 {{-- 
         <div class="dropdown">
           <button class="btn btn-light-lg" type="button" id="dropdownMenuSizeButton4" data-toggle="dropdown" 
           aria-haspopup="true" aria-expanded="false" _msttexthash="313989" _msthash="279"> Dicipline </button>
-<<<<<<< HEAD
         </div>        
-=======
-        </div>
-         --}}
->>>>>>> 111e19fff1907e77c835374c364e6d247aa491f1
         {{-- Extraction de données --}}
 
         {{-- <div class="dropdown">
@@ -594,13 +403,8 @@
           <li class="nav-item"> <a class="nav-link" href="pages/samples/error-500.html"> Bricoles</a></li>
         </ul>
       </div>
-<<<<<<< HEAD
     </li>
 
-=======
-    </li> --}}
- 
->>>>>>> 111e19fff1907e77c835374c364e6d247aa491f1
     {{-- Paramètres --}}
    
   
@@ -648,16 +452,9 @@
       </a>
       <div class="collapse" id="error">
         <ul class="nav flex-column sub-menu">
-<<<<<<< HEAD
           <li class="nav-item"> <a class="nav-link" href="{{url('/modifparam')}}"> Table des paramètres </a></li>
           <li class="nav-item"> <a class="nav-link" href="#"> Modifier les bornes de l'exercice </a></li>
 
-=======
-          {{-- <li class="nav-item"> <a class="nav-link" href="#"> Table des paramètres </a></li>
-          <li class="nav-item"> <a class="nav-link" href="#"> Modifier les bornes de l'exercice </a></li>
-           --}}
-          {{-- <div class="dropdown">
->>>>>>> 111e19fff1907e77c835374c364e6d247aa491f1
            <li class="nav-item">
             <button class="btn btn-light-lg dropdown-toggle" type="button" id="dropdownMenuSizeButton1" data-toggle="dropdown" 
             aria-haspopup="true" aria-expanded="false" _msttexthash="313989" _msthash="279"> Op. Ouverture </button>
@@ -669,7 +466,6 @@
               <a class="dropdown-item" href="#" _msttexthash="22987" _msthash="285" _mstvisible="1">Changement de trimestre</a>
             </div>
            </li>
-<<<<<<< HEAD
 
           <li class="nav-item"> <a class="nav-link" href="{{url('/confimpression')}}"> Configurer Imprimante </a></li>
           <li class="nav-item"> <a class="nav-link" href="{{url('/changetrimestre')}}"> Changement de trimestre </a></li>
@@ -714,21 +510,5 @@
       <a href="{{url('/')}}" class="btn btn-primary mr-2">Déconnexion</a>
     </div>  
   </div>
-=======
-          </div> --}}
-          {{-- <li class="nav-item"> <a class="nav-link" href="{{url('/confimpression')}}"> Configurer Imprimante </a></li>
-          <li class="nav-item"> <a class="nav-link" href="{{url('/changetrimestre')}}"> Changement de trimestre </a></li> --}}
-          <li class="nav-item"> <a class="nav-link" href="{{url('/frais')}}">Frais mensuel et <br>année academique </a></li>
-          <li class="nav-item"> <a class="nav-link" href="{{url('/connexiondonnees')}}">Connexion à la<br>base de donnée </a></li>
-          <li class="nav-item"> <a class="nav-link" href="{{url('/paramsfacture')}}">Paramètre Facture </a></li>
-          <li class="nav-item"> <a class="nav-link" href="{{url('/inscriptions')}}">Enregistrement utilisateur</a></li>
-
-        </ul>
-      </div>
-    </li> 
-  </ul>
-  
-  
->>>>>>> 111e19fff1907e77c835374c364e6d247aa491f1
 
 </nav>
