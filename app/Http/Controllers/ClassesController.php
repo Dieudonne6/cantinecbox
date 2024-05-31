e9bce0581a5a2908e4f1abfb63f37603c77457fc
@Franck229 <dieudonneayena6@gmail.com>
2024-05-31 15:28:42 +0100

    facture en ligne

--- app/Http/Controllers/ClassesController.php
+++ app/Http/Controllers/ClassesController.php
@@ -660,26 +660,7 @@ public function savepaiementcontrat(Request $request) {
 
 
         
-        public function create()
-        {
-            $qrCodeString = Session::get('qrCodeString');
 
-      $qrCode = new QrCode($qrCodeString);
-      $qrCode->setSize(300);
-      $qrCode->setMargin(20); 
-      $qrCode->setEncoding('UTF-8');
-      $qrCode->setWriterByName('png');
-      $qrCode->setErrorCorrectionLevel(ErrorCorrectionLevel::HIGH());
-      $qrCode->setForegroundColor(['r' => 0, 'g' => 0, 'b' => 0, 'a' => 0]);
-      $qrCode->setBackgroundColor(['r' => 255, 'g' => 255, 'b' => 255, 'a' => 0]);
-      $qrCode->setLogoSize(200, 200);
-      $qrCode->setValidateResult(false);  
-      $qrCode->setRoundBlockSize(true);
-      $qrCode->setWriterOptions(['exclude_xml_declaration' => true]);
-      header('Content-Type: '.$qrCode->getContentType());
-      $qrCode->writeFile('D:/PROJETS/CBOX/cantinecbox/public/qrcode');
-      return redirect()->route('pdffacture');
-        }
 
 
     public function telechargerfacture() {
