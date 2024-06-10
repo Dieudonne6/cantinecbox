3dbe843f24d4cf7f80cff3451ea6a1f60faa7b0e
cboxone <logossoumichel@gmail.com>
2024-06-10 19:23:47 +0100

    cc

--- resources/views/pages/etatpaiement1.blade.php
+++ resources/views/pages/etatpaiement1.blade.php
@@ -26,7 +26,7 @@
 
                 <div class="col">
                     <!-- Bouton de soumission de formulaire -->
-                    <label for="debut" style="visibility: hidden">supprimer paiememtn</label>
+                    <label for="debut" style="visibility: hidden">supprimer paiement</label>
                     <button type="submit" class="btn btn-primary w-100">Rechercher</button>
                 </div>
                 {{-- <div class="col">
@@ -151,13 +151,9 @@
                                         <a type="button" style="height: 45px" class="btn btn-primary w-50 me-1" href="{{ url('imprimerfiche/' . $resultatsIndividuel['id_paiementcontrat']) }}">
                                         Imprimer fiche
                                         </a>
-                                    <form
-                                        action="{{ url('supprimerpaiement/' . $resultatsIndividuel['id_paiementcontrat']) }}"
-                                        method="post">
-                                        @csrf
-                                        @method('DELETE')
-                                        <input type="submit" class="btn btn-danger w-100" value="Supprimer">
-                                    </form>
+                                        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                            Supprimer
+                                          </button> 
                                 </div>
                             </td>
                         </tr>
@@ -170,6 +166,32 @@
 
         {{-- @endif --}}
     </div>
+
+    <!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Confirmation de suppression</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          Êtes-vous sûr de vouloir supprimer cette ligne ?
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
+          <form action="{{ url('supprimerpaiement/' . $resultatsIndividuel['id_paiementcontrat']) }}"
+                                        method="post">
+                                        @csrf
+                                        @method('DELETE')
+                                        <input type="submit" class="btn btn-danger" value="Supprimer">
+            </form>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+
 @endsection
 
 <script>
