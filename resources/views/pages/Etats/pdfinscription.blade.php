680a5f71938652de134d5c27909936e2e7dca8f4
Pascaline Dah Zinsou <pascaline@wazoom-studio.com>
2024-06-14 18:51:39 +0100

    Made somes change.

--- resources/views/pages/Etats/pdfinscription.blade.php
+++ resources/views/pages/Etats/pdfinscription.blade.php
@@ -13,8 +13,8 @@
         <div id="contenu">
 
         </div>
-            <div class="col-6 contenu"  style="background-color:white;">
-              <div class="d-flex justify-content-center mb-5">
+            <div class="col-6 mx-auto contenu"  style="background-color:white;">
+              <div class="d-flex justify-content-between mb-5">
                 <div>
                   @if($logoUrl)
                   <img src="{{ asset('storage/logo/' . $logoUrl) }}" alt="Logo" class="logoimg">
@@ -22,7 +22,7 @@
                       <p>Aucun logo disponible.</p>
                   @endif
                 </div>
-                <div style="width: 75%;">
+                <div>
                   <h5 style="padding-top:20px;">
                     COMPLEXE SCOLAIRE: 
                   </h5> 
@@ -35,7 +35,7 @@
                     Reçu d'inscription cantine
                 </h5><br>
 
-                    <div class="row">
+                    <div class="row mx-auto">
                         <div class="col-5">
                             <p style="font-weight:bold;">Date de paiement :</p><br>
                         </div>
@@ -43,7 +43,7 @@
                             <p>{{ $dateContrat }}</p><br>
                         </div>
                     </div>
-                    <div class="row">
+                    <div class="row mx-auto">
                       <div class="col-5">
                           <p style="font-weight:bold;">Eleve :</p><br>
                       </div>
@@ -51,7 +51,7 @@
                           <p>{{ $elevyo }}</p><br>
                       </div>
                   </div>
-                    <div class="row">
+                    <div class="row mx-auto">
                         <div class="col-5">
                             <p style="font-weight:bold;">Montant paiement :</p><br>
                         </div>
@@ -61,7 +61,7 @@
                     </div>
                   
                   
-                    <div class="row">
+                    <div class="row mx-auto">
                         <div class="col-5">
                             <p style="font-weight:bold;">Classe :</p><br>
                         </div>
@@ -69,7 +69,7 @@
                             <p>{{ $classe }}</p><br>
                         </div>
                     </div>
-                    <div class="row">
+                    <div class="row mx-auto">
                         <div class="col-5">
                             <p style="font-weight:bold;">Signature Caissier</p><br>
                         </div>
@@ -77,7 +77,7 @@
                             <p style="font-weight:bold;">Signature Parent</p><br>
                         </div>
                     </div>
-                    <div class="row">
+                    <div class="row mx-auto">
                         <div class="col-5">
                             <p style="font-weight:bold;"></p><br><br>
                         </div>
@@ -99,6 +99,8 @@
         var page = window.open();
         page.document.write('<html><head><title>fiche</title>');
         page.document.write('<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" >');
+        page.document.write('<style>@media print { .logoimg {width: 35%; margin-top: 20px; } }</style>');
+
         page.document.write('</head><body>');
             page.document.write(document.querySelector('.contenu').innerHTML);
             
