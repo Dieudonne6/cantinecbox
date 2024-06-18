5762b4d73c033f0d1f05be6cf2d4461b41d6d488
cboxone <logossoumichel@gmail.com>
2024-06-18 17:23:50 +0100

    cc

--- resources/views/pages/etat/filteretat.blade.php
+++ resources/views/pages/etat/filteretat.blade.php
@@ -2,7 +2,31 @@
 
 @extends('layouts.master')
 @section('content')
+<style>
+    /* Styles spécifiques pour l'impression sur papier A4 */
+    @media print and (size: A4) {
+        table {
+            font-size: 5pt; /* Ajuste la taille de la police pour A4 */
+        }
+        th, td {
+            padding: 3px; /* Ajuste le padding pour A4 */
+        }
+    }
 
+    /* Optimisation des marges et padding pour l'écran et l'impression */
+    table {
+        width: 100%;
+        border-collapse: collapse;
+    }
+    th, td {
+        border: 1px solid #ddd;
+        padding: 4px; /* Ajuste le padding pour une meilleure lisibilité */
+        font-size: 10pt; /* Taille de police pour l'écran */
+    }
+    th {
+        background-color: #f2f2f2;
+    }
+</style>
 
 <body>
   
@@ -14,7 +38,7 @@
           {{csrf_field()}}
           <div class="form-group row">
             <div class="col-lg-3">
-              <select class="form-control w-100"  name="annee">
+              <select class="form-control w-100" name="annee">
                 <option value="">Sélectionnez une année</option>
                 @foreach ($anne as $annees)
                 <option value="{{$annees}}">{{$annees}}</option>
@@ -22,150 +46,119 @@
               </select>
             </div>
             <div class="col-lg-3">
-              <select class="form-control w-100"  name="classe">
+              <select class="form-control w-100" name="classe">
                 <option value="">Sélectionnez une classe</option>
-                
                 @foreach ($class as $classes)
                 <option value="{{$classes->CODECLAS}}">{{$classes->CODECLAS}}</option>
                 @endforeach
               </select>
             </div>
             <div class="col-lg-2">
-              <button type="submit" class="btn btn-primary">
-                Afficher
-              </button>
+              <button type="submit" class="btn btn-primary">Afficher</button>
             </div>
-            
             <div class="col-lg-2 offset-2">
-              {{-- <a class="telecharger btn btn-success" href="{{ url('/facturenormalise/' . $nomcompleteleve) }}" target="_blank">Imprimer</a> --}}
-              {{-- 
-                <a type="button" class="btn btn-primary"  href="{{ url('/impressionetatdroitconstate/') }}" target="_blank">
-                  Imprimer
-                </a> --}}
-                <button onclick="imprimerPage()" class="btn btn-primary" >Imprimer</button>
-              </div>
+              <button onclick="imprimerPage()" class="btn btn-primary">Imprimer</button>
             </div>
-          </form>
-          
-          
-          <div id="contenu" class="tables">
-            <div>
-              <h4 class="card-title" style="text-align: center; font-weight:bold;">Etats des droits constatés ANNEE-ACADEMIQUE: {{ $annee }} - {{ $anneesuivant }} | CLASSE: {{ $classe }}</h4>
-            </div><br>
-            <div class="table-responsive pt-3">
-              <table id="myTable" class="table table-bordered">
-                <thead>
-                  <tr>
-                    <th>
-                      N
-                    </th>
-                    <th>Elève</th>
-                    <th>Classe</th>
+          </div>
+        </form>
+        
+        <div id="contenu" class="tables">
+          <div>
+            <h4 class="card-title" style="text-align: center; font-weight:bold;">Etats des droits constatés ANNEE-ACADEMIQUE: {{ $annee }} - {{ $anneesuivant }} | CLASSE: {{ $classe }}</h4>
+          </div><br>
+          <div >
+            <table id="myTable">
+              <thead>
+                <tr>
+                  <th>N</th>
+                  <th>Elève</th>
+                  <th>Classe</th>
+                  @foreach ($moisContrat as $mois)
+                  @if ($mois->nom_moiscontrat != 'Juillet' && $mois->nom_moiscontrat != 'Aout')
+                  <th>{{ $mois->nom_moiscontrat }}</th>
+                  @endif
+                  @endforeach         
+                  <th>Total</th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach ($eleves as $index => $eleve)
+                <tr>
+                    <td>{{ $index + 1 }}</td>
+                    <td style="width: 10px">{{ $eleve->NOM }} {{ $eleve->PRENOM }}</td>
+                    <td>{{ $eleve->CODECLAS }}</td>
+                    
+                    @php
+                    $totalMois = [];
+                    @endphp
                     @foreach ($moisContrat as $mois)
                     @if ($mois->nom_moiscontrat != 'Juillet' && $mois->nom_moiscontrat != 'Aout')
-                    <th>{{ $mois->nom_moiscontrat }}</th>
+                    @php
+                    $totalMois[$mois->id_moiscontrat] = 0;
+                    $montantTotalMois = 0;
+                    $paiementTrouve = false;
+                    @endphp
+                    <td>
+                        @foreach ($eleve->contrats as $contrat)
+                        @foreach ($contrat->paiements as $paiement)
+                        @if ($paiement->mois_paiementcontrat == $mois->id_moiscontrat)
+                        <!-- Afficher la date et le montant si le paiement correspond au mois -->
+                        {{ \Carbon\Carbon::parse($paiement->date_paiementcontrat)->format('d/m/Y') }}</br></br>
+                        {{ $paiement->montant_paiementcontrat }}
+                        @php
+                        $montantTotalMois += $paiement->montant_paiementcontrat;
+                        $paiementTrouve = true;
+                        @endphp
+                        @endif
+                        @endforeach
+                        @endforeach
+                        @if (!$paiementTrouve)
+                        @if ($mois->id_moiscontrat != 13)
+                        Pas inscrit
+                        @else
+                        0
+                        @endif
+                        @php
+                        $totalMois[$mois->id_moiscontrat] += $montantTotalMois;
+                        @endphp
+                        @else
+                        @php
+                        $totalMois[$mois->id_moiscontrat] += $montantTotalMois;
+                        @endphp
+                        @endif
+                        
+                    </td>
                     @endif
-                    @endforeach         
-                    <th>Total</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  @foreach ($eleves as $index => $eleve)
-                  <tr>
-                      <td>{{ $index + 1 }}</td>
-                      <td>{{ $eleve->NOM }} {{ $eleve->PRENOM }}</td>
-                      <td>{{ $eleve->CODECLAS }}</td>
-                      
-                      @php
-                      $totalMois = [];
-                      @endphp
-                      @foreach ($moisContrat as $mois)
-                      @if ($mois->nom_moiscontrat != 'Juillet' && $mois->nom_moiscontrat != 'Aout')
-                      @php
-                      $totalMois[$mois->id_moiscontrat] = 0;
-                      $montantTotalMois = 0;
-                      $paiementTrouve = false;
-                      @endphp
-                      <td>
-                          @foreach ($eleve->contrats as $contrat)
-                          @foreach ($contrat->paiements as $paiement)
-                          @if ($paiement->mois_paiementcontrat == $mois->id_moiscontrat)
-                          <!-- Afficher la date et le montant si le paiement correspond au mois -->
-                          {{ \Carbon\Carbon::parse($paiement->date_paiementcontrat)->format('d/m/Y') }}</br></br>
-                          {{ $paiement->montant_paiementcontrat }}
-                          @php
-                          $montantTotalMois += $paiement->montant_paiementcontrat;
-                          $paiementTrouve = true;
-                          @endphp
-                          @endif
-                          @endforeach
-                          @endforeach
-                          @if (!$paiementTrouve)
-                          @if ($mois->id_moiscontrat != 13)
-                          Pas inscrit
-                          @else
-                          0
-                          @endif
-                          @php
-                          $totalMois[$mois->id_moiscontrat] += $montantTotalMois;
-                          @endphp
-                          @else
-                          @php
-                          $totalMois[$mois->id_moiscontrat] += $montantTotalMois;
-                          @endphp
-                          @endif
-                          
-                      </td>
-                      @endif
-                      @endforeach
-                      <td>
-                          {{ array_sum($totalMois) }}
-                      </td>
-                  </tr>
-                  @endforeach
-              </tbody>
-              
-              </table>
-            </div>
+                    @endforeach
+                    <td>{{ array_sum($totalMois) }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+            </table>
           </div>
         </div>
       </div>
     </div>
-  </body>
-  @endsection
+  </div>
+</body>
+@endsection
 
-  <script>
-    
-    function imprimerPage() {
-      var table = document.getElementById('myTable');
-      table.classList.remove('dataTable');
-      
-      var page = window.open('', 'blank_');
-        page.document.write('<html><head><title>EDC_annee_{{ $annee }}_{{ $anneesuivant }}_classe_{{ $classe }}</title>');
-        // page.document.write('<html><head><title>Imprimer</title>');
-        // page.document.write('<link rel="stylesheet" href="https://cdn.datatables.net/2.0.7/css/dataTables.dataTables.css" />');
-        page.document.write('<style>@media print { .dt-end { display: none !important; } }</style>');
-       
-        page.document.write('<style>@media print { .dt-start { display: none !important; } }</style>');
-
-        // page.document.write('<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" >');
-        page.document.write('<style>@media print { th { display: table-cell !important; } }</style>');
-        page.document.write('<style>@media print { th:nth-child(n+1), td:nth-child(n+1) {width: 20px; max-width: 20px; overflow: hidden; font-size: 0.8rem;} }</style>');
-        page.document.write('<style>@media print {');
-        page.document.write('table { width: 100%; border-collapse: collapse; }');
-        page.document.write('th, td { border-top: 1px solid black; border-bottom: 1px solid black; padding: 4px; font-size: 10px; }');
-        page.document.write('tr:nth-child(even) { background-color: #f2f2f2; }'); // Couleur de fond pour les lignes paires
-        page.document.write('tr:nth-child(odd) { background-color: #ffffff; }'); // Couleur de fond pour les lignes impaires
-        
-        page.document.write('thead { display: table-header-group; }');
-        page.document.write('tr, td { page-break-inside: avoid; }');
-        page.document.write('body { margin: 0; }');
-        page.document.write('</style>');
-        page.document.write('</head><body>');
-        page.document.write(document.getElementById('contenu').innerHTML);
-        page.document.write('</body></html>');
-        page.document.close();
-        page.print();
-        page.close();
-    }
-    </script>
+<script>
+function imprimerPage() {
+  var table = document.getElementById('myTable');
+  table.classList.remove('dataTable');
+  
+  var page = window.open('', 'blank_');
+  page.document.write('<html><head><title>EDC_annee_{{ $annee }}_{{ $anneesuivant }}_classe_{{ $classe }}</title>');
+  page.document.write('<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" >');
+  page.document.write('<style>@media print { .dt-end { display: none !important; } }</style>');
+  page.document.write('<style>@media print { .dt-start { display: none !important; } }</style>');
+  page.document.write('<style>table { width: 100%; border-collapse: collapse; } th, td { border: 1px solid #ddd; padding: 4px; } tbody tr:nth-child(even) { background-color: #f1f3f5; } tbody tr:nth-child(odd) { background-color: #ffffff; } </style>');
+  page.document.write('</head><body>');
+  page.document.write(document.getElementById('contenu').innerHTML);
+  page.document.write('</body></html>');
+  page.document.close();
+  page.print();
+  page.close();
+}
+</script>
