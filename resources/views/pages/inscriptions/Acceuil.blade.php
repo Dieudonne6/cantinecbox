047ebb87f65e9093e22635ecc2609a1079406bca
cboxone <logossoumichel@gmail.com>
2024-06-21 16:22:04 +0100

    fr

--- resources/views/pages/inscriptions/Acceuil.blade.php
+++ resources/views/pages/inscriptions/Acceuil.blade.php
@@ -118,7 +118,7 @@
 
 <!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-xl">
+  <div class="modal-dialog modal-lg">
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
@@ -127,64 +127,75 @@
 
        <div class="col-12">
         <nav>
-          <div class="nav nav-tabs" id="nav-tab" role="tablist">
-            <button class="nav-link active" id="nav-Infor-tab" data-bs-toggle="tab" data-bs-target="#nav-Infor" type="button" role="tab" aria-controls="nav-Infor" aria-selected="true">Informations généraless</button>
+          <div class="nav nav-tabs" id="nav-tab" role="tablist" style="font-size: 14px;">
+            <button class="nav-link active" id="nav-Infor-tab" data-bs-toggle="tab" data-bs-target="#nav-Infor" type="button" role="tab" aria-controls="nav-Infor" aria-selected="true">Informations générales</button>
             <button class="nav-link" id="nav-Detail-tab" data-bs-toggle="tab" data-bs-target="#nav-Detail" type="button" role="tab" aria-controls="nav-Detail" aria-selected="false">Détail des notes</button>
             <button class="nav-link" id="nav-Deta-tab" data-bs-toggle="tab" data-bs-target="#nav-Deta" type="button" role="tab" aria-controls="nav-Deta" aria-selected="false">Détails des paiements</button>
             <button class="nav-link" id="nav-finan-tab" data-bs-toggle="tab" data-bs-target="#nav-finan" type="button" role="tab" aria-controls="nav-finan" aria-selected="false">Informations financières</button>
-            <button class="nav-link" id="nav-Emploi-tab" data-bs-toggle="tab" data-bs-target="#nav-Emploi" type="button" role="tab" aria-controls="nav-Emploi" aria-selected="Emploi du temps">Emploi du temps</button>
+            <button class="nav-link" id="nav-Emploi-tab" data-bs-toggle="tab" data-bs-target="#nav-Emploi" type="button" role="tab" aria-controls="nav-Emploi" aria-selected="false">Emploi du temps</button>
             <button class="nav-link" id="nav-Position-tab" data-bs-toggle="tab" data-bs-target="#nav-Position" type="button" role="tab" aria-controls="nav-Position" aria-selected="false">Position Enseignants</button>
-          </div>
+          </div>         
         </nav>
         <div class="tab-content" id="nav-tabContent">
           <!--  -->
           <div class="tab-pane fade show active" id="nav-Infor" role="tabpanel" aria-labelledby="nav-Infor-tab" tabindex="0">
-            <form class="accordion-body">                    
-              <div class="mb-2">
-                <input type="" class="form-control" id="date" placeholder="Date de Naissance">
+            <form class="accordion-body col-md-6 mx-auto" style="background-color: #f0eff3">  
+              
+              <div class="form-group row mt-2">
+                <label for="exampleInputMobile" class="col-sm-4 col-form-label">Date de Naissance</label>
+                <div class="col-sm-6">
+                  <input type="text" class="form-control" id="date" placeholder="jj/mm/dd">
+                </div>
               </div>
-              <div class="mb-2">
-                <input type="" class="form-control" id="lieu" placeholder="Lieu de Naissance">
+              
+              <div class="form-group row">
+                <label for="exampleDropdownFormDate" class="col-sm-4 col-form-label">Lieu de Naissance</label>
+                <div class="col-sm-6">
+                  <input type="text" class="form-control" id="lieu" placeholder="Lieu de Naissance">
+                </div>
               </div>
-              <div class="mb-2">
-                <label for="exampleDropdownFormDate" class="form-label p-2">Sexe</label>
+
+             
+                <label for="exampleDropdownFormDate" class="col-sm-2 col-form-label">Sexe</label>
                 <select class="form-auto">
-                  <option _msttexthash="42861" _msthash="226">Masculin</option>
-                  <option _msttexthash="73437" _msthash="227">Féminin</option>
+                  <option>Masculin</option>
+                  <option>Féminin</option>
+                </select>
+             
+                <label for="exampleDropdownFormPassword2" class="col-sm-3 col-form-label">Types élèves</label>
+                <select class="form-auto">
+                  <option>Ancien</option>
+                  <option>Nouveau</option>
+                </select>
+            
+              <div class="form-group row mt-4">
+                <label for="exampleDropdownFormDate" class="col-sm-4 col-form-label">Date d'inscription</label>
+                <div class="col-sm-6">
+                  <input type="" class="form-control" id="date" placeholder="Date d'inscription">
+                </div>
+              </div>
+              <div>
+                <label for="exampleDropdownFormP" class="col-sm-4 col-form-label">Apte</label>
+                <select class="form-auto">
+                  <option>Oui</option>
+                  <option>Non</option>
                 </select>
               </div>
-              <div class="mb-2">
-                <label for="exampleDropdownFormPassword2" class="form-label">Types élèves</label>
-                <select class="form-auto">
-                  <option _msttexthash="42861" _msthash="226">Ancien</option>
-                  <option _msttexthash="73437" _msthash="227">Nouveau</option>
-                </select>
-              </div>
-              <div class="mb-2">
-                <input type="" class="form-control" id="date" placeholder="Date d'inscription">
-              </div>
-              <div class="mb-2 p-2">
-                <label for="exampleDropdownFormPassword2" class="form-label">Apte</label>
-                <select class="form-auto">
-                  <option >Oui</option>
-                  <option >Non</option>
-                </select>
-              </div>
-              <div class="mb-2">
-                <div class="form-check-center mx-9">
-                  <input type="checkbox" class="form-check-input mx-9" id="dropdownCheck2">
+              <div class="col-md-10">
+                <div class="col-form-check-center mx-6">
+                  <input type="checkbox" class="form-check-input mx-6" id="dropdownCheck2">
                   <label class="form-check-label" for="dropdownCheck2">
                     Statut Redoublant
                   </label>
                 </div>
               </div>                           
-          </form> 
+            </form> 
           </div>
           <!--  -->
           <div class="tab-pane fade" id="nav-Detail" role="tabpanel" aria-labelledby="nav-Detail-tab" tabindex="0">
-            <form class="accordion-body">
-              <div class="table-responsive" style="height: 100px; overflow: auto;">
-                <table class="table table-bordered table-striped" style="min-width: 800px;">
+            <form class="accordion-body col-md-6 mx-auto">
+              <div class="table-responsive mt-2">
+                <table class="table table-bordered table-striped">
                   <thead>
                     <tr>
                       <th scope="col">Matière</th>
@@ -198,13 +209,13 @@
                   </thead>
                   <tbody>
                     <tr>
-                      <td>1</td>
-                      <td>Mark</td>
-                      <td>Otto</td>
-                      <td>@mdo</td>
-                      <td>Jacob</td>
-                      <td>Thornton</td>
-                      <td>@fat</td>
+                      <td>Français</td>
+                      <td>09</td>
+                      <td>15</td>
+                      <td>06</td>
+                      <td>10</td>
+                      <td>T13</td>
+                      <td>16</td>
                     </tr>
                     <tr>
                       <td>2</td>
@@ -219,112 +230,119 @@
                 </table>
               </div>
                
-              <div class="row">
-                <div class="mb-2">
-                  <label for="exampleDropdownForm">Moy.1</label>
-                  <input type="" class="form-control" id="moy1" >
-                </div>
-                <div class="mb-2">
-                  <label for="exampleDropdownForm">Moy.2</label>
-                  <input type="" class="form-control" id="moy2">
-                </div>
-                <div class="mb-2">
-                  <label for="exampleDropdownForm">Moy.3</label>
-                  <input type="" class="form-control" id="moy3">
-                </div>
-              </div>
-
+               <div class="table-responsive mt-2">
+                
+                    <table class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <td>Moy 1</td>
+                                <td>Moy 2</td>
+                                <td>Moy 3</td>
+                                <td>Moy Totale</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th>12</th>
+                                <th>14</th>
+                                <th>13</th>
+                                <th>11</th>
+                            </tr>
+                        </tbody>
+                    </table>
+                
+               </div>
             </form>
           </div>
           <!--  -->
           <div class="tab-pane fade" id="nav-Deta" role="tabpanel" aria-labelledby="nav-Deta-tab" tabindex="0">
-            <div class="accordion-body">  
-              <div class="form-check-center mx-9">
-                  <input type="checkbox" class="form-check-input mx-9" id="dropdownCheck2">
-                  <label class="form-check-label" for="dropdownCheck2">
-                    Détail des composantes
-                  </label>
-              </div> 
-
+            <div class="accordion-body col-md-6 mx-auto">
+              
+                <input type="checkbox" class="form-check-input" id="Check">
+                <label class="col-6 form-check-label" for="Check">
+                  Détail des composantes
+                </label>
+              
+            
               <div class="table-responsive mt-2" style="height: 100px; overflow: auto;">
-                  <table class="table table-bordered " style="min-width: 10px;">
-                    <thead>
-                      <tr>
-                        <th>n°Recu</th>
-                        <th>Date</th>
-                        <th>Montant</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>130</td>
-                        <td>05/06/2024</td>
-                        <td>190 000</td>
-                      </tr>
-                      <tr>
-                        <td> </td>
-                        <td> </td>
-                        <td> </td> 
-                      </tr>
-                    </tbody>
-                  </table>
+                <table class="table table-bordered" style="min-width: 10px;">
+                  <thead>
+                    <tr>
+                      <th>n°Recu</th>
+                      <th>Date</th>
+                      <th>Montant</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>130</td>
+                      <td>05/06/2024</td>
+                      <td>190 000</td>
+                    </tr>
+                    <tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                    </tr>
+                  </tbody>
+                </table>
               </div>
-
+            
               <div class="input-group p-2">
                 <div class="input-group-prepend">
                   <span class="input-group-text">Somme</span>
                 </div>
-                <input type="text" class="form-control" aria-label="Montant (au dollar près)" _mstaria-label="669448" _msthash="174">
+                <input type="text" class="form-control" aria-label="Montant (au dollar près)">
               </div>
-
-              <div class="input-group">
+            
+              <div class="input-group p-2">
                 <div class="input-group-prepend">
                   <span class="input-group-text">Reste à payer</span>
                 </div>
-                <input type="text" class="form-control" aria-label="Montant (au dollar près)" _mstaria-label="669448" _msthash="174">
+                <input type="text" class="form-control" aria-label="Montant (au dollar près)">
               </div>
-
+            
               <button type="button" class="btn btn-outline-danger btn-icon-text p-2">
-                <i class="typcn typcn-upload btn-icon-prepend"></i><font> Imprimer récapitulatif des paiements </font>
+                <i class="typcn typcn-upload btn-icon-prepend"></i> Imprimer récapitulatif des paiements
               </button>
-              
+            
               <div class="card-body">
-                <h4 class="card-title" _msttexthash="323960" _msthash="106">Réduction Montants dus</h4>
-                <h8><form class="forms-sample">
-                  <div class="form-group row">
-                    <label for="exampleInputUsername2" class="col-sm-6 p-2 col-form-label">Scolarité</label>
+                <h4 class="card-title">Réduction Montants dus</h4>
+                <form class="forms-sample">
+                  <div class="form-group row p-2">
+                    <label for="scolarite" class="col-sm-6 col-form-label">Scolarité</label>
                     <div class="col-sm-6">
-                      <input type="num" class="form-control" id="exampleInputUsername2" >
+                      <input type="number" class="form-control" id="scolarite">
                     </div>
                   </div>
-                  <div class="form-group row">
-                    <label for="exampleInputEmail2" class="col-sm-6 col-form-label">Arrièré</label>
+                  <div class="form-group row p-2">
+                    <label for="arriere" class="col-sm-6 col-form-label">Arriéré</label>
                     <div class="col-sm-6">
-                      <input type="num" class="form-control" id="exampleInputEmail2" >
+                      <input type="number" class="form-control" id="arriere">
                     </div>
                   </div>
-                  <div class="form-group row">
-                    <label for="exampleInputMobile" class="col-sm-6 col-form-label">Frais 1</label>
+                  <div class="form-group row p-2">
+                    <label for="frais1" class="col-sm-6 col-form-label">Frais 1</label>
                     <div class="col-sm-6">
-                      <input type="num" class="form-control" id="exampleInputMobile">
+                      <input type="number" class="form-control" id="frais1">
                     </div>
                   </div>
-                  <div class="form-group row">
-                    <label for="exampleInputPassword2" class="col-sm-6 col-form-label">Frais 2</label>
+                  <div class="form-group row p-2">
+                    <label for="frais2" class="col-sm-6 col-form-label">Frais 2</label>
                     <div class="col-sm-6">
-                      <input type="num" class="form-control" id="exampleInputPassword2">
+                      <input type="number" class="form-control" id="frais2">
                     </div>
                   </div>
-                  <div class="form-group row">
-                    <label for="exampleInputConfirmPassword2" class="col-sm-6 col-form-label">Frais 3</label>
+                  <div class="form-group row p-2">
+                    <label for="frais3" class="col-sm-6 col-form-label">Frais 3</label>
                     <div class="col-sm-6">
-                      <input type="num" class="form-control" id="exampleInputConfirmPassword2">
+                      <input type="number" class="form-control" id="frais3">
                     </div>
                   </div>
-                </form></h8>
+                </form>
               </div>
-
             </div>
+            
           </div>
           <!--  -->
           <div class="tab-pane fade" id="nav-finan" role="tabpanel" aria-labelledby="nav-finan-tab" tabindex="0">
