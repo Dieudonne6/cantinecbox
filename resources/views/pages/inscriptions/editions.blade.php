cae903866a25d94ed16db9f6e7155c98cb38e19f
@Franck229 <dieudonneayena6@gmail.com>
2024-07-15 11:20:38 +0100

    modif avant conge

--- resources/views/pages/inscriptions/editions.blade.php
+++ resources/views/pages/inscriptions/editions.blade.php
@@ -44,13 +44,13 @@
        
           
         <button><a href="{{ url('/listedesclasses') }}"><i class="fas fa-list"></i> Liste des classes</a></button>
-        <button><a href="{{ url('/certificatsdescolarite') }}"><i class="fas fa-certificate"></i> Certificats de scolarité</a></button>
+        <button><a href="{{ url('/certificat') }}"><i class="fas fa-certificate"></i> Certificats de scolarité</a></button>
         <button><a href="{{ url('/attestationsdescolarite') }}"><i class="fas fa-file-alt"></i> Attestations de scolarité</a></button>
         <button><a href="{{ url('/enquetesstatistiques') }}"><i class="fas fa-chart-bar"></i> Enquêtes statistiques</a></button>
         <button><a href="{{ url('/etatdelacaisse') }}"><i class="fas fa-cash-register"></i> État de la caisse</a></button>
         <button><a href="{{ url('/pointderecouvrement') }}"><i class="fas fa-chalkboard-teacher"></i> Point de recouvrement par enseignement</a></button>
         <button><a href="{{ url('/etatdesrecouvrements') }}"><i class="fas fa-file-invoice-dollar"></i> État des recouvrements</a></button>
-        <button><a href="{{ url('/etatdesdroits') }}"><i class="fas fa-file-signature"></i> État des droits constatés par classe</a></button>
+        <button><a href="{{ url('/droitconstate') }}"><i class="fas fa-file-signature"></i> État des droits constatés par classe</a></button>
         <button><a href="{{ url('/situationfinanciere') }}"><i class="fas fa-calendar-alt"></i> Situation financière selon l'échéancier</a></button>
         <button><a href="{{ url('/listedesretardsdepaiement') }}"><i class="fas fa-exclamation-circle"></i> Liste des retards de paiement</a></button>
         <button><a href="{{ url('/lettresderelance') }}"><i class="fas fa-envelope"></i> Lettres de relance</a></button>
