ed0a967498732a08c8f10aeaff2e71f169d2495b
Dieudonne6 <carineams21@gmail.com>
2025-02-20 13:59:27 +0100

    version actuelle scodelux 20 FEVRIER 2K25 Francky

--- app/Models/Facturescolarit.php
+++ app/Models/Facturescolarit.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class facturescolarit extends Model
+class Facturescolarit extends Model
 {
     use HasFactory;
     protected $table = 'facturescolarit';
