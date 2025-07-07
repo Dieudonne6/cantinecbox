ff0793236c999ea82d0542bd5edaabf64b67f324
Mahzouz22 <mahzouzams8@gmail.com>
2025-07-07 12:02:48 +0100

    Correction des migrations pour rapport annuel

--- database/migrations/2025_07_07_105526_decision_config_annuel_table.php
+++ database/migrations/2025_07_07_105526_decision_config_annuel_table.php
@@ -15,15 +15,15 @@ return new class extends Migration
     {
         Schema::create('decision_config_annuel', function (Blueprint $table) {
             $table->increments('id');
-            $table->decimal('seuil_Passage', 8, 2);
-            $table->decimal('Min_Cycle1', 8, 2);
-            $table->decimal('Min_Cycle2', 8, 2);
+            $table->decimal('seuil_Passage', 4, 2);
+            $table->decimal('Min_Cycle1', 4, 2);
+            $table->decimal('Min_Cycle2', 4, 2);
             $table->string('Promotion');
             $table->boolean('Statut')->default(0);
             $table->enum('StatutF', ['P', 'X', 'R'])->default('P');
-            $table->decimal('Seuil_Felicitations', 8, 2);
-            $table->decimal('Seuil_Encouragements', 8, 2);
-            $table->decimal('Seuil_tableau_Honneur', 8, 2);
+            $table->decimal('Seuil_Felicitations', 4, 2);
+            $table->decimal('Seuil_Encouragements', 4, 2);
+            $table->decimal('Seuil_tableau_Honneur', 4, 2);
         });
     }
 
