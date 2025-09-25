6291c919d10499963ec07795275008176bb2c6c7
cboxone <logossoumichel@gmail.com>
2025-09-25 10:50:45 +0100

    login et mots de passe auto

--- app/Models/Usercontrat.php
+++ app/Models/Usercontrat.php
@@ -8,6 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Usercontrat extends Model
 {
     use HasFactory;
+
     protected $table = 'usercontrat';
+    protected $primaryKey = 'id_usercontrat';
     public $timestamps = false;
+
+    // Colonnes qu’on peut remplir avec create() ou update()
+    protected $fillable = [
+        'nom_usercontrat',
+        'prenom_usercontrat',
+        'login_usercontrat',
+        'password_usercontrat',
+        'statut_usercontrat',
+    ];
 }
